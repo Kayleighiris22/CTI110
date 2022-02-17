ff4fdc29f4c70aa394268542e5d9b03eb40bdc18
Kayleighiris22 <99447923+Kayleighiris22@users.noreply.github.com>
2022-02-16 19:30:59 -0500

    Add files via upload

---
+++ P1HW2_CanfieldKayleigh.py
@@ -0,0 +1,42 @@
+# File:  P1HW2_CanfieldKayleigh.py
+
+# A brief description of the project
+# Date
+# CTI-110 P1HW2 - Pizza Order
+# Your Name
+#
+
+#parse input as float
+students = float(input("How many students do you want to order pizza for?"))
+
+#obtain slices = students * 3
+slices = (students) * (3);
+
+#obtain pizzas = students / 2
+pizzas = (students) / 2;
+
+print("----Pizza Order--------")
+
+    
+#print number of student after converting to string
+print("Number of Students :   " + str(students))
+
+#print pizza slices needed after converting to string
+print("Pizza Slices Needed:   " + str(slices))
+
+#print pizzas needed after converting to string
+print("Pizzas Needed :   " + str(pizzas))
+    
+print("---------------------------")
+
+#call function P1HW2_CanfieldKayleigh.py()
+#P1HW2_CanfieldKayleigh.py()
+
+
+
+    
+
+    
+    
+    
+
